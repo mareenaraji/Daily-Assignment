fccd4202d1fd0bf87a26fb291efb56a7c8aeb16a
mareenaraji <120193434+mareenaraji@users.noreply.github.com>
2023-05-08 10:26:14 +0530

    Add files via upload

---
+++ day 24/EFDemo1/EFDemo1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Adding 1 row
+
+           /* using (var context = new EMPDEntities())
+            {
+                var std = new Salesman_Master()
+                {
+                   SalesManno="S00006",
+                   SalesManname="Christy",
+                   Address1="ED/98",
+                   Address2="Dream",
+                   City="Idukki",
+                   Pincode=678954,
+                   State="Kerala",
+                   Salamt=78909,
+                   Tgttoget=987,
+                   Ytdsales=42,
+                   Remarks="Good"
+                };
+                context.Salesman_Master.Add(std);
+            
+                context.SaveChanges();
+            }*/
+
+            // updating a row
+
+            /*using (var context = new EMPDEntities())
+            {
+                var std = context.Salesman_Master.First<Salesman_Master>();
+                std.SalesManname = "Steve";
+                context.SaveChanges();
+            }*/
+
+           //deleting a row
+
+            using (var context = new EMPDEntities())
+            {
+                var std = context.Salesman_Master.Where(y=>y.SalesManno="S0002);
+                context.Salesman_Master.Remove(std);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ day 24/SQLQuery1.sql
@@ -0,0 +1,7 @@
+select * from Client_Master;
+
+select * from Product_Master;
+
+select * from Salesman_Master;
+
+select * from Sales_Order_Details;
