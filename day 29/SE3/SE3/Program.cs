23e48c06a9bcbc1ff0d4fb80dc10acf43603ac08
mareenaraji <120193434+mareenaraji@users.noreply.github.com>
2023-05-17 09:27:37 +0530

    Add files via upload

---
+++ day 29/SE3/SE3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Exercise027
+{
+    public static void Main()
+    {
+        int number;
+        int sum = 0;
+
+        do
+        {
+            Console.Write("Number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            sum = number + sum;
+
+            if (number != 0)
+                Console.WriteLine("Total: {0}", sum);
+        }
+        while (number != 0);
+
+        Console.WriteLine("Finished");
+    }
+}
---
+++ day 29/SampleExercise2/SampleExercise2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Exercise025
+{
+    public static void Main()
+    {
+        int num, multiplier = 1;
+
+        Console.Write("Insert a number to multiply: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        while (multiplier <= 10)
+        {
+            Console.WriteLine("{0} x {1} = {2}",
+                num, multiplier, num * multiplier);
+            multiplier++;
+        }
+    }
+}
