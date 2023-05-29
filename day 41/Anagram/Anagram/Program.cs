d29086380179e518bd9a084b2a9d5bcac9f888f0
mareenaraji <120193434+mareenaraji@users.noreply.github.com>
2023-05-29 09:27:08 +0530

    Add files via upload

---
+++ day 41/Anagram/Anagram/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace Anagram_quetsion
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Enter the string");
+
+            string str1 = Console.ReadLine();
+
+            Console.WriteLine("Enter the second string");
+
+            string str2 = Console.ReadLine();
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+
+            {
+
+                Console.WriteLine("Both the strings are Anagrams");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Both the strings are not Anagrams");
+
+            }
+
+        }
+
+    }
+
+}
---
+++ day 41/Mareena/AbstractClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+abstract class Book
+{
+    protected string title;
+    public abstract void SetTitle(string s);
+    public string GetTitle()
+    {
+        return title;
+    }
+}
+class MyBook : Book
+{
+    public override void SetTitle(string s)
+    {
+        title = s;
+    }
+}
+
+class MainClass
+
+{
+    public static void Main(string[] args)
+    {
+        string str = Console.ReadLine();
+        MyBook myBook = new MyBook();
+        myBook.SetTitle(str);
+        Console.WriteLine("The title is: " + myBook.GetTitle());
+    }
+
+}
