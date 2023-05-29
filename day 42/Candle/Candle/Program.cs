a5e1e2912fa690ed44dd370f632f26a6b629dd76
mareenaraji <120193434+mareenaraji@users.noreply.github.com>
2023-05-29 09:27:59 +0530

    Add files via upload

---
+++ day 42/Candle/Candle/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candle
+{
+    class Result
+
+    {
+        public static int birthdayCakeCandles(List<int> candles)
+        {
+            int max = candles.Max();
+            int count = candles.Count(c => c == max);
+            return count;
+        }
+    }
+    class Solution
+    {
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+                List<int> candles = Console.ReadLine().TrimEnd().Split(' ').Select(int.Parse).ToList();
+                if (candles.Count != candlesCount)
+                {
+                    throw new ArgumentException("Invalid input: Number of candles does not match the provided count.");
+                }
+                int result = Result.birthdayCakeCandles(candles);
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+        }
+    }
+}
---
+++ day 42/Mareena/Ratio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ratio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                int positiveCount = 0;
+                int negativeCount = 0;
+                int zeroCount = 0;
+                foreach (int num in arr)
+                {
+                    if (num > 0)
+                        positiveCount++;
+                    else if (num < 0)
+                        negativeCount++;
+                    else
+                        zeroCount++;
+                }
+                double positiveRatio = (double)positiveCount / n;
+                double negativeRatio = (double)negativeCount / n;
+                double zeroRatio = (double)zeroCount / n;
+                Console.WriteLine(positiveRatio.ToString("F6"));
+                Console.WriteLine(negativeRatio.ToString("F6"));
+                Console.WriteLine(zeroRatio.ToString("F6"));
+
+            }
+
+        }
+
+    }
+}
