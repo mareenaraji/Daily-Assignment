a9177a18dc253db586ef6b91bd693efd3878a534
mareenaraji <120193434+mareenaraji@users.noreply.github.com>
2023-06-08 17:14:45 +0530

    Add files via upload

---
+++ day 50/Azure Demos/MRTestWebAPIAZ/MRTestWebAPIAZ/Controllers/customerController1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MRTestWebAPIAZ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class customerController1 : ControllerBase
+    {
+        // GET: api/<customerController1>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "christy", "adhithya" };
+        }
+
+        // GET api/<customerController1>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<customerController1>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<customerController1>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<customerController1>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
