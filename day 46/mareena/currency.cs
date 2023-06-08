9bc3b148b9fac3b8d5446a71629806ec5e9b8051
mareenaraji <120193434+mareenaraji@users.noreply.github.com>
2023-06-08 17:07:16 +0530

    Add files via upload

---
+++ day 46/mareena/currency.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace currency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double amount =34567.76;
+
+            CultureInfo usCulture = new CultureInfo("en-US");
+            CultureInfo indiaCulture = new CultureInfo("en-IN");
+            CultureInfo chinaCulture = new CultureInfo("zh-CN");
+            CultureInfo franceCulture = new CultureInfo("fr-FR");
+
+            string usFormatted = amount.ToString("C", usCulture);
+            string indiaFormatted = amount.ToString("C", indiaCulture);
+            string chinaFormatted = amount.ToString("C", chinaCulture);
+            string franceFormatted = amount.ToString("C", franceCulture);
+
+            Console.WriteLine("US: " + usFormatted);
+            Console.WriteLine("India: " + indiaFormatted);
+            Console.WriteLine("China: " + chinaFormatted);
+            Console.WriteLine("France: " + franceFormatted);
+        }
+    }
+}
