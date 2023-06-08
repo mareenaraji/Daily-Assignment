dc649add7ac25f3337985ef8c02cdabd38c6e64a
mareenaraji <120193434+mareenaraji@users.noreply.github.com>
2023-06-08 17:04:51 +0530

    Add files via upload

---
+++ day 44/ArrayList/ArrayList/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // Number of lines
+
+            int[][] lines = new int[n][];
+            for (int i = 0; i < n; i++)
+
+            {
+                string[] lineData = Console.ReadLine().Split(' ');
+                int count = int.Parse(lineData[0]);
+                lines[i] = new int[count];
+                for (int j = 0; j < count; j++)
+
+                {
+
+                    lines[i][j] = int.Parse(lineData[j + 1]);
+
+                }
+
+            }
+            int q = int.Parse(Console.ReadLine()); // Number of queries
+            for (int i = 0; i < q; i++)
+
+            {
+                string[] queryData = Console.ReadLine().Split(' ');
+                int x = int.Parse(queryData[0]);
+                int y = int.Parse(queryData[1]);
+
+                if (x >= 1 && x <= n && y >= 1 && y <= lines[x - 1].Length)
+
+                {
+                    Console.WriteLine(lines[x - 1][y - 1]);
+                }
+                else
+                {
+                    Console.WriteLine("ERROR!");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ day 44/bigdecimal/bigdecimal/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bigdecimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of string");
+            int n = int.Parse(Console.ReadLine());
+            string[] strarr = new string[n];
+            Console.WriteLine("Enter the array:");
+
+
+            for (int i = 0; n < strarr.Length; i++)
+            {
+                strarr[i] = Console.ReadLine();
+            }
+        }
+    }
+}
